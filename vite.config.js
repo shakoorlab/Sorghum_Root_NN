484148a673020a533bf32341de71f02ca2aa39ec
JaccobStanton <jstanton23@my.stlcc.edu>
2024-04-24 12:16:31 -0500

    Added proxy to temporary bypass CORS issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+const BACKEND_URL = process.env.BACKEND_URL;
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Proxying API requests
+      "/api": {
+        target: BACKEND_URL,
+        changeOrigin: true, // needed for virtual hosted sites
+      },
+    },
+  },
+});
